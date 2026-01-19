1d894549322b8184648a0185f53a9999998873e0
AshishGadiya7 <ashishashish13600@gmail.com>
2026-01-19 16:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (59.57%) - LeetSync

--- 1720-crawler-log-folder/crawler-log-folder.cpp
+++ 1720-crawler-log-folder/crawler-log-folder.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int res=0;
+        stack<int>s;
         for(int i=0;i<logs.size();i++){
             if(logs[i]=="../"){
-                if(res!=0)res--;
+                if(!s.empty())s.pop();
             }
             else if(logs[i]=="./")continue;
             else{
-                res++;
+                s.push(i);
             }
-        }return res;
+        }return s.size();
     }
 };
