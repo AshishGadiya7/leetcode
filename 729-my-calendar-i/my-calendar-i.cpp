74311fec25a7785ca47b2ab8f9f800620cddef36
AshishGadiya7 <ashishashish13600@gmail.com>
2026-01-20 21:33:19 +0530

    Time: 105 ms (18.64%) | Memory: 49.1 MB (10.03%) - LeetSync

---
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -0,0 +1,31 @@
+class MyCalendar {
+public:
+map<int,int>mp;
+    MyCalendar() {
+        
+    }
+    
+    bool book(int startTime, int endTime) {
+          mp[startTime]++;
+          mp[endTime]--;
+          int temp=0;
+          for(auto it:mp){
+             temp+=it.second;
+             if(temp>=2){
+            mp[startTime]--;
+                mp[endTime]++;
+                if (mp[startTime] == 0) mp.erase(startTime);
+                if (mp[endTime] == 0) mp.erase(endTime);
+            return false;
+          }
+             
+          }
+         return true;
+    }
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(startTime,endTime);
+ */
