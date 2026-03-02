c8291423f9339787f019324008c0bfd1faaefc26
AshishGadiya7 <ashishashish13600@gmail.com>
2026-03-02 21:04:26 +0530

    Time: 2 ms (27.55%) | Memory: 9.7 MB (32.12%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int>mp;
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        stack<char>sta;
+        vector<int>visited(26,0);
+        for(int i=0;i<n;i++){
+            if(!visited[s[i]-'a']){
+                 while(!sta.empty()){
+                    if(mp[sta.top()]!=0 && sta.top()>s[i]){
+                        visited[sta.top()-'a']=0;
+                        sta.pop();
+                    }else{
+                        visited[s[i]-'a']=1;
+                        sta.push(s[i]);
+                        break;
+                    }
+                 }
+                 if(sta.empty()){
+                    visited[s[i]-'a']=1;
+                    sta.push(s[i]);
+                 }mp[s[i]]--;
+
+            }else{
+                mp[s[i]]--;
+            }
+        }
+        string res="";
+        while(!sta.empty()){
+            res+=sta.top();
+            sta.pop();
+        }reverse(res.begin(),res.end());
+        return res;
+    }
+};
